7d7aedb51a90699c8081467d7ab5cfcf4eea5aa6
unknown <amomen@gmail.com>
2024-08-07 16:33:50 +0330

    pgpool doc

--- pgpool/4.5.2 Ubuntu 22.04/fix socket glitch on Ubuntu, add vip automatic mount/script/cluster_vip.sh
+++ pgpool/4.5.2 Ubuntu 22.04/fix socket glitch on Ubuntu, add vip automatic mount/script/cluster_vip.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DEVICE=$(ip -br link | awk '$1 != "lo" {print $1}' | tail -1)
-PRIMARY_NODE=$(echo $(sudo -iu postgres pcp_node_info -h localhost -U pgpool -w | head -$(sudo -iu postgres pcp_node_info -h localhost -U pgpool -w | awk '{print $8}' | grep -n primary | cut -d":" -f1) | tail -1 | cut -d" " -f1))
+PRIMARY_NODE=$(echo $(pcp_node_info -h localhost -U pgpool -w | head -$(pcp_node_info -h localhost -U pgpool -w | awk '{print $8}' | grep -n primary | cut -d":" -f1) | tail -1 | cut -d" " -f1))
 
 # Command to be executed
-cmd="ssh -T -i ~/.ssh/id_rsa_pgpool ${PRIMARY_NODE} /usr/bin/sudo /sbin/ip addr add 172.23.124.74/24 dev ${DEVICE} label ${DEVICE}:0"
+cmd="ssh -T -i ~/.ssh/id_rsa_pgpool ${PRIMARY_NODE} /sbin/ip addr add 172.23.124.74/24 dev ${DEVICE} label ${DEVICE}:0"
 
 # Execute the command and capture the output
 output=$($cmd 2>&1)
@@ -12,7 +12,7 @@ exit_code=$?
 echo $exit_code
 
 if [ "$PRIMARY_NODE" == "$(hostname)" ]; then
-	cmd="ssh -T -i ~/.ssh/id_rsa_pgpool $(echo $(sudo -iu postgres pcp_node_info -h localhost -U pgpool -w | head -$(sudo -iu postgres pcp_node_info -h localhost -U pgpool -w | awk '{print $8}' | grep -n primary | cut -d":" -f1) | tail -1 | cut -d" " -f1)) /etc/pgpool2/scripts/escalation.sh"
+	cmd="ssh -T -i ~/.ssh/id_rsa_pgpool $(echo $(pcp_node_info -h localhost -U pgpool -w | head -$(pcp_node_info -h localhost -U pgpool -w | awk '{print $8}' | grep -n primary | cut -d":" -f1) | tail -1 | cut -d" " -f1)) /etc/pgpool2/scripts/escalation.sh"
 	eval $cmd
 fi
 
