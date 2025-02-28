656fb8ef438d39cb711387648616b710f24beaad
amomen9 <51079948+amomen9@users.noreply.github.com>
2025-03-01 00:20:31 +0330

    Update pg_class report.sql
    Added child and parent indexes

--- usefull scripts/pg_class report.sql
+++ usefull scripts/pg_class report.sql
@@ -1,28 +1,33 @@
-select 
-	c.oid,
-	c.relname,
-	ns.nspname "Schema Name",
-	ty.typname "Type Name",
-	ofty.typname "Composite Type Name",
-	a.rolname "Owner Name",
-	am.amname "Access Method Name",
-	c.relfilenode,
-	ts.spcname "Tablespace Name",
-	c3.relname "Index Parent Table",
-	c2.relname "Toast Relname",
-	rw.rulename "Rewrite Rulename"
-from pg_class c 
-left join pg_namespace ns	on c.relnamespace = ns.oid
-left join pg_type ty		on c.reltype=ty.oid
-left join pg_type ofty		on c.reloftype=ofty.oid
-left join pg_authid a		on c.relowner=a.oid
-left join pg_am am			on c.relam=am.oid
-left join pg_tablespace ts	on c.reltablespace = ts.oid
-left join pg_class c2		on c.reltoastrelid=c2.oid
-left join pg_rewrite rw		on c.relrewrite=rw.oid
-left join pg_index i		on i.indexrelid=c.oid
-left join pg_class c3		on i.indrelid=c3.oid
---where c.relname in ('customers','idx_60550_pk_customers')
-order by oid asc
-
-
+SELECT 
+    c.oid,
+    c.relname,
+    ns.nspname AS "Schema Name",
+    ty.typname AS "Type Name",
+    ofty.typname AS "Composite Type Name",
+    a.rolname AS "Owner Name",
+    am.amname AS "Access Method Name",
+    c.relfilenode,
+    ts.spcname AS "Tablespace Name",
+    cindex2parent.relname AS "Index Parent Table",
+    STRING_AGG(cparent2index.relname, ', ') AS "Table Indexes",
+    ctoast.relname AS "Toast Relname",
+    rw.rulename AS "Rewrite Rulename"
+FROM pg_class c
+LEFT JOIN pg_namespace ns ON c.relnamespace = ns.oid
+LEFT JOIN pg_type ty ON c.reltype = ty.oid
+LEFT JOIN pg_type ofty ON c.reloftype = ofty.oid
+LEFT JOIN pg_authid a ON c.relowner = a.oid
+LEFT JOIN pg_am am ON c.relam = am.oid
+LEFT JOIN pg_tablespace ts ON c.reltablespace = ts.oid
+LEFT JOIN pg_class ctoast ON c.reltoastrelid = ctoast.oid
+LEFT JOIN pg_rewrite rw ON c.relrewrite = rw.oid
+LEFT JOIN pg_index iparent ON iparent.indexrelid = c.oid
+LEFT JOIN pg_class cindex2parent ON iparent.indrelid = cindex2parent.oid
+LEFT JOIN pg_index ichild ON ichild.indrelid = c.oid
+LEFT JOIN pg_class cparent2index ON ichild.indexrelid = cparent2index.oid
+--WHERE c.relname IN ('customers', 'idx_60550_pk_customers')
+GROUP BY 
+    c.oid, c.relname, ns.nspname, ty.typname, ofty.typname, 
+    a.rolname, am.amname, c.relfilenode, ts.spcname, 
+    cindex2parent.relname, ctoast.relname, rw.rulename
+ORDER BY c.oid ASC;
