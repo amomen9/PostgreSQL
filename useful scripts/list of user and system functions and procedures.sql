383840ee026ab252dcbb652cbaebb2bb4b013049
amomen9 <51079948+amomen9@users.noreply.github.com>
2025-03-09 22:35:28 +0330

    Create list of user and system functions and procedures.sql

---
+++ useful scripts/list of user and system functions and procedures.sql
@@ -0,0 +1,22 @@
+SELECT 
+    p.proname AS function_name,
+    n.nspname AS schema_name,
+    pg_catalog.pg_get_function_result(p.oid) AS return_type,
+    pg_catalog.pg_get_function_arguments(p.oid) AS argument_types,
+    l.lanname AS language,
+    p.prosrc AS source_code,
+    p.protrftypes AS transformed_return_type_oids,
+    CASE 
+        WHEN p.prokind = 'f' THEN 'Function'
+        WHEN p.prokind = 'a' THEN 'Aggregate Function'
+        WHEN p.prokind = 'p' THEN 'Procedure'
+        WHEN p.prokind = 'w' THEN 'Window Function'
+        ELSE 'Unknown'
+    END AS function_kind,
+    p.probin AS binary_code,
+    p.prosqlbody AS sql_body
+FROM pg_proc p
+JOIN pg_namespace n ON p.pronamespace = n.oid
+JOIN pg_language l ON p.prolang = l.oid
+--where p.proname like '%func or proc%'
+ORDER BY n.nspname, p.proname;
